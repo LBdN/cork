c3160865fc6930834b9648f14dc400beaea7a865
lionel barret <lionel.barret@gmail.com>
2016-03-02 22:56:30 +0100

    still very messy :/ not done yet

--- win/dllcork/src/dllmain.cpp
+++ win/dllcork/src/dllmain.cpp
@@ -8,27 +8,22 @@
 namespace ma {
 
 
-	Arithmetic::Arithmetic() {
-		//TODO: Initialize items here
-	}
-	Arithmetic::~Arithmetic() {
-		//TODO: Release unused items here
-	}
+
 
 	//FUNCTION: adds two values
-	float Arithmetic::addition(float& val_1, float& val_2) {
+	float addition(float val_1, float val_2) {
 		return val_1 + val_2;
 	}
 	//FUNCTION: substracts two values
-	float Arithmetic::substraction(float& val_1, float& val_2) {
+	float substraction(float val_1, float val_2) {
 		return val_1 - val_2;
 	}
 	//FUNCTION: multiplies two values
-	float Arithmetic::multiplication(float& val_1, float& val_2) {
+	float multiplication(float val_1, float val_2) {
 		return val_1 * val_2;
 	}
 	//FUNCTION: divide two values
-	float Arithmetic::division(float& val_1, float& val_2) {
+	float division(float val_1, float val_2) {
 		if (val_2 == 0)
 			throw new std::invalid_argument("denominator cannot be 0");
 		return val_1 / val_2;
--- win/unitycork/main.cs
+++ win/unitycork/main.cs
@@ -13,17 +13,25 @@ namespace unitycork
 
 
         //Lets make our calls from the Plugin
-        [DllImport ( "dllcork" )]
+        [DllImport ( "dllcork2", EntryPoint = "addition" )]
         public static extern float addition ( float val_1, float val_2 );
-        [DllImport ( "dllcork" )]
+        [DllImport ( "dllcork2" )]
         private static extern float substraction ( float val_1, float val_2 );
-        [DllImport ( "dllcork" )]
+        [DllImport ( "dllcork2" )]
         private static extern float multiplication ( float val_1, float val_2 );
-        [DllImport ( "dllcork" )]
+        [DllImport ( "dllcork2" )]
         private static extern float division ( float val_1, float val_2 );
+
+        //Lets make our calls from the Plugin
+        [DllImport ( "dllcork2", EntryPoint = "testcork" )]
+        public static extern float testcork (  );
+
 
         public static void Start ()
         {
+
+            Debug.Print ( testcork ( ).ToString () );
+            var i = addition ( 5, 5 );
             Debug.Print ( addition ( 5, 5 ).ToString() );
             Debug.Print ( substraction ( 10, 5 ).ToString () );
             Debug.Print ( multiplication ( 2, 5 ).ToString () );
